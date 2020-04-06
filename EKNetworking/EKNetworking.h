fb7d4d2e5dfb587688834261a7872ca4b9c2795d
Emil Karimov <emvakar@gmail.com>
2020-04-06 13:04:55 +0300

    [spm] add spm support

--- EKNetworking/EKNetworking.h
+++ EKNetworking/EKNetworking.h
@@ -15,5 +15,3 @@ FOUNDATION_EXPORT double EKNetworkingVersionNumber;
 FOUNDATION_EXPORT const unsigned char EKNetworkingVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <EKNetworking/PublicHeader.h>
-
-
