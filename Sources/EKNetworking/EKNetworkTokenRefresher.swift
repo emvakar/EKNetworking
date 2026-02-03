5c9c0b07927c18ad13c42cd0d0d1c24a3ff1e721
Egor Solovev <egorsolovev@Egors-MacBook-Air.local>
2026-02-03 10:03:22 +0300

    Add swift concurrency for EKNetworkTokenRefresher

--- Sources/EKNetworking/EKNetworkTokenRefresher.swift
+++ Sources/EKNetworking/EKNetworkTokenRefresher.swift
@@ -15,12 +15,49 @@ public protocol EKNetworkTokenRefresherProtocol {
 
     // Обновление токена при 5012 ошибке для ДСС токена
     func refreshDSSAuthToken(completion: @escaping (EKNetworkError?) -> Void)
+    
+    // Async versions
+    @available(iOS 13.0, macOS 10.15, *)
+    func refreshAuthToken() async throws
+    
+    @available(iOS 13.0, macOS 10.15, *)
+    func refreshDSSAuthToken() async throws
 
 }
 
 public extension EKNetworkTokenRefresherProtocol {
 
     // Дефолтная реализация обновление токена ДСС
-    func refreshDSSAuthToken(completion: @escaping (EKNetworkError?) -> Void) { completion(nil) }
+    func refreshDSSAuthToken(completion: @escaping (EKNetworkError?) -> Void) {
+        completion(nil)
+    }
+    
+    // Default async implementation that wraps completion-based version
+    @available(iOS 13.0, macOS 10.15, *)
+    func refreshAuthToken() async throws {
+        try await withCheckedThrowingContinuation { (continuation: CheckedContinuation<Void, Error>) in
+            refreshAuthToken { error in
+                if let error = error {
+                    continuation.resume(throwing: error)
+                } else {
+                    continuation.resume(returning: ())
+                }
+            }
+        }
+    }
+    
+    // Default async implementation for DSS
+    @available(iOS 13.0, macOS 10.15, *)
+    func refreshDSSAuthToken() async throws {
+        try await withCheckedThrowingContinuation { (continuation: CheckedContinuation<Void, Error>) in
+            refreshDSSAuthToken { error in
+                if let error = error {
+                    continuation.resume(throwing: error)
+                } else {
+                    continuation.resume(returning: ())
+                }
+            }
+        }
+    }
 
 }
